bb5359f3a94f9a259096404faaebefb0356d6c23
tarian22 <basitolatunji18@gmail.com>
2025-03-17 11:30:05 -0700

    Added new files and updates

---
+++ script.js
@@ -0,0 +1,13 @@
+// let moved = false;
+
+// function moveImage() {
+//     const img = document.getElementById("movingImage");
+    
+//     if (!moved) {
+//         img.style.transform = "translateX(100px)"; // Move right
+//     } else {
+//         img.style.transform = "translateX(0)"; // Move back
+//     }
+    
+//     moved = !moved; // Toggle movement
+// }
